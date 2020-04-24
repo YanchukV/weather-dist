9d1472e036cf2d5acf271cc405550126bcadfac4
Vyacheslav <jantschuk@gmail.com>
2020-04-24 13:32:02 +0300

    dist

---
+++ precache-manifest.e59374b3898de37336b8bf201ab96719.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcd21e8a37ffa086c1eb86baa42544e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5e126f9e3dedcdb3605",
+    "url": "./static/css/main.9b821a8c.chunk.css"
+  },
+  {
+    "revision": "9ea50fafe4869ed4c60c",
+    "url": "./static/js/2.32ee2aa0.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "./static/js/2.32ee2aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e126f9e3dedcdb3605",
+    "url": "./static/js/main.1247d39b.chunk.js"
+  },
+  {
+    "revision": "c9eaee3bb320a97943a9",
+    "url": "./static/js/runtime-main.947d8cef.js"
+  },
+  {
+    "revision": "06825fa10944e31000ccb9183e6dad41",
+    "url": "./static/media/bg.06825fa1.png"
+  },
+  {
+    "revision": "eabf94951d3364dd79218e9c0fb84230",
+    "url": "./static/media/weather-icon-cloudy.eabf9495.png"
+  },
+  {
+    "revision": "608d6a01fe1677642d86e521a98cf3b4",
+    "url": "./static/media/weather-icon-rainy.608d6a01.png"
+  },
+  {
+    "revision": "5e112c6173192573527ab2ada4f6eed7",
+    "url": "./static/media/weather-icon-sunny.5e112c61.png"
+  }
+]);
